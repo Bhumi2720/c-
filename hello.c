f4518267fb3571a564c47020b667a755e8ee49e7
Bhumi2720 <bhumii2704@gmail.com>
2025-09-01 23:02:54 +0530

    Added file hello

---
+++ hello.c
@@ -0,0 +1,105 @@
+
+  // int a=5;
+  // a<5?printf(" hello"): printf(" hiii");
+
+ // int a= 3;
+  //switch (a)
+// {
+  //case 1:
+ // printf("hello");
+  // break; 
+  //case 2:
+  //printf("hiii");
+   //break;
+ //default:
+  //printf(" bye");
+   //break;
+  //}
+
+
+//  int a;
+  //printf( " enter the number ");
+  //scanf( "%d", &a);
+  
+ // switch (a)
+  //{
+  //case 1:
+//  printf(" monday ");
+//  break;
+//  case 2:
+  //printf (" tuesday");
+//  break;
+  // case 3:
+  // printf( " wenesday");
+  // break;
+  // case 4:
+  //  printf(" thursday");
+  //    break;
+  // case 5:
+  //   printf(" friday");
+  //   break;
+  // case 6 :
+  //   printf(" saturday");
+  //   break;
+  // case 7:
+  //   printf ( " sunday");
+     //break;
+   //default:
+    // printf( " in valid ");
+    // break;
+   // }
+
+
+   //#include<stdio.h>
+   //int main (){
+   //int a,b;
+  // float c;
+  // int ch;
+  // printf( " enter two numbers");
+  // scanf("%d,%d", &a,&b);
+  // printf ("\n1: add \n2: subtract\n3: multiply\n4: divide");
+  // printf ( " enter your choice");
+  // scanf(" %d",&ch);
+  // switch (ch){
+   //   case 1:
+   //   c= a+b;
+   //   printf("sum=%f",c);
+   //   break;
+   //   case 2:
+   //   c= a-b;
+   //   printf("sub=%f", c);
+   //   break;
+   //    case 3:
+   //   c=a*b;
+   //   printf("multiply=%f", c);
+   //   break;
+   //   case 4:
+   //   c= (float) a/b;
+   //   printf(" divide=%f",c);
+   //   break;
+   //   default:
+   //   printf(" invalid ") ;
+   //   break;
+   //} 
+   
+   #include<stdio.h>
+   int main(){
+    int ch;
+    float amount , converted;
+    char ch;
+    printf( " amount we want to convert");
+    scanf( " %f", &amount)
+    printf( " ") ;
+    switch (ch)
+    {
+    case ' $':
+      /* code */
+      break;
+    
+    default:
+      break;
+    }  
+
+
+    return 0;
+   }
